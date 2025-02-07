d5906bc1b4668cfdf7e3ccc1887cc7422d8b47c5
AliceTaylors <159647480+AliceTaylors@users.noreply.github.com>
2025-02-07 20:58:31 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,25 @@
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import useTranslation from 'next-translate/useTranslation';
 
 export default function Footer() {
-  const { t } = useTranslation('common');
-  
   return (
     <div style={{ 
       marginTop: '70px', 
       marginBottom: '20px',
       opacity: 0.4
     }}>
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center'
-      }}>
-        <a 
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <a
           href="https://t.me/CHECKERCC"
           target="_blank"
           rel="noopener noreferrer"
-          style={{
-            padding: '8px',
+          style={{ 
+            padding: '8px', 
             fontSize: '14px',
             color: '#fff',
             textDecoration: 'none',
@@ -29,17 +28,11 @@ export default function Footer() {
             gap: '8px'
           }}
         >
-          <FontAwesomeIcon icon={faMessage} /> 
-          Join Telegram Chat
+          <FontAwesomeIcon icon={faMessage} /> Join Telegram Chat
         </a>
       </div>
-      <div style={{ 
-        fontSize: '15px', 
-        textAlign: 'center',
-        marginTop: '10px',
-        color: '#fff'
-      }}>
-        SECCX.PRO © {new Date().getFullYear()}
+      <div style={{ fontSize: '15px', textAlign: 'center' }}>
+        <span>SECCX.PRO @ 2022~{new Date().getFullYear()}</span>
       </div>
     </div>
   );
