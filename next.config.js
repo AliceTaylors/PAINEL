f3680834afd81ffe43c3028366fc1e3d2544e967
AliceTaylors <159647480+AliceTaylors@users.noreply.github.com>
2025-02-07 09:00:45 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,24 +29,7 @@ module.exports = nextTranslate({
       {
         source: '/api/affiliate/stats/:id',
         destination: '/api/affiliate/stats/[id]'
-      },
-      {
-        source: '/seccx.php',
-        destination: '/api/seccx'
       }
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, token' },
-        ],
-      },
     ];
   }
 });
