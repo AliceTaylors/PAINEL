ec8ba54c653f95f83fb3d5986bb7afc85b3dfa72
AliceTaylors <159647480+AliceTaylors@users.noreply.github.com>
2025-02-08 00:51:40 -0300

    Update User.js

--- src/pages/api/models/User.js
+++ src/pages/api/models/User.js
@@ -1,23 +1,28 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from 'mongoose';
 
-mongoose.Promise = global.Promise;
-
-const customerSchema = new Schema({
+const userSchema = new mongoose.Schema({
   login: String,
-  password: { type: String, select: false },
-  admin: { type: Boolean, default: false },
-  ipAddress: { type: String, select: false },
-  balance: { type: Number, min: 0 },
-  logs: [{ history_type: String, cost: Number, data: String }],
-  order: {
-    amount: { type: Number, default: 0 },
-    complete: { type: Boolean, default: false },
-    currency: { type: String },
-    address: { type: String },
-    pricing: { type: String },
-    externalId: { type: String, default: "" },
+  password: String,
+  balance: {
+    type: Number,
+    default: 0
   },
-  cards: [String],
+  logs: {
+    type: Array,
+    default: []
+  },
+  mail: String,
+  ipAddress: String,
+  order: {
+    amount: Number,
+    complete: Boolean,
+    currency: String,
+    address: String,
+    pricing: String,
+    externalId: String,
+  },
+  adyen_block_until: Date,
+  premium_block_until: Date
 });
 
-module.exports = mongoose.models.User || mongoose.model("User", customerSchema);
+export default mongoose.models.User || mongoose.model('User', userSchema); 
