4c7aad0761b009ebbb531a158756acfa277e9547
AliceTaylors <159647480+AliceTaylors@users.noreply.github.com>
2025-02-08 11:15:27 -0300

    Update cpf.js

--- src/pages/api/consulta/cpf.js
+++ src/pages/api/consulta/cpf.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../../../utils/mongodb';
+import dbConnect from '../../../utils/dbConnect';
 import axios from 'axios';
 
 export default async function handler(req, res) {
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { db } = await connectToDatabase();
+    const db = await dbConnect();
     const { cpf } = req.body;
     
     // Verificar token e usuário
